fab6c460fc993d7e1f9616ad286cb6d394996847
mohdraqif <mohdraqif94@gmail.com>
2020-03-23 14:50:59 +0530

    Intro started

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import './App.css'
 import Footer from './components/Footer'
 import Header from './components/Header'
+import Carousel from './components/Carousel'
+import Intro from './components/Intro'
 
 export default class App extends Component {
   render() {
     return (
       <div> 
         <Header/>
+        {/* <Carousel/> */}
+        <Intro/>
         <Footer/>
       </div>
     );
---
+++ src/components/Intro.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../App.css'
+
+const Intro = () => {
+  return (
+    <div>
+      <section class="intro">  
+        <div class="content">
+          <h1>You can create full screen sections without javascript.</h1>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Intro;
