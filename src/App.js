ae6746020b7db4cfc8236a58cf4773af9c4d8451
mohdraqif <mohdraqif94@gmail.com>
2020-03-24 22:17:34 +0530

    Gallery added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import Intro from './components/Intro'
+import Gallery from './components/Gallery'
 
 export default class App extends Component {
   render() {
@@ -10,6 +11,7 @@ export default class App extends Component {
       <div> 
         <Header/>
         <Intro/>
+        <Gallery/>
         <Footer/>
       </div>
     );
---
+++ src/components/Gallery.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Gallery = () => {
+  return (
+    <div className='GALLERY'>
+      <h1>Tech we deal in good</h1>
+      <i class="fa fa-chevron-down fa-2x"></i>
+      <div className="gallery">
+        <div class="container">
+          <a href="/"><div class="box"><img alt="" src="https://images.unsplash.com/photo-1488190211105-8b0e65b80b4e?w=500&h=500&fit=crop" width="290" height="410"/></div></a>
+        
+          <a href="/"><div class="box"><img alt="" src="https://images.unsplash.com/photo-1488190211105-8b0e65b80b4e?w=500&h=500&fit=crop" width="290" height="410"/></div></a>
+        
+          <a href="/"><div class="box"><img alt="" src="https://images.unsplash.com/photo-1488190211105-8b0e65b80b4e?w=500&h=500&fit=crop" width="290" height="410"/></div></a>
+        
+          <a href="/"><div class="box"><img alt="" src="https://images.unsplash.com/photo-1488190211105-8b0e65b80b4e?w=500&h=500&fit=crop" width="290" height="410"/></div></a>
+        
+          <a href="/"><div class="box"><img alt="" src="https://images.unsplash.com/photo-1488190211105-8b0e65b80b4e?w=500&h=500&fit=crop" width="290" height="410"/></div></a>
+        
+          <a href="/"><div class="box"><img alt="" src="https://images.unsplash.com/photo-1488190211105-8b0e65b80b4e?w=500&h=500&fit=crop" width="290" height="410"/></div></a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Gallery
