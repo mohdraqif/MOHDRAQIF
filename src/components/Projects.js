b463864f4b07d397ccac139d5f62ff39589ebc53
mohdraqif <mohdraqif94@gmail.com>
2020-03-30 22:21:19 +0530

    Intro Completed

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,12 +6,13 @@ const Projects = () => {
   <section class="section">
   <div class="banner"><h2>Our Best Projects</h2></div>
     <div class="grid">
-      <div class="item item--medium"></div>
-      <div class="item item--medium"></div>
-      <div class="item item--medium"></div>
-      <div class="item item--medium"></div>
-      <div class="item item--medium"></div>
-      <div class="item item--medium"></div>
+      <div class="item item--medium" title="Tech Store"></div>
+      <div class="item item--medium" title="Beach Resort"></div>
+      <div class="item item--medium" title="Vintage Technologies"></div>
+      <div class="item item--medium" title="To-Do List"></div>
+      <div class="item item--medium" title="Get Linked"></div>
+      <div class="item item--medium" title="Budget Calculator"></div>
+      <a href="/" class="fancy-button bg-gradient1"><span>all projects</span></a>
     </div>
   </section>
   );
