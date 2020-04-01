50512a434e648c3a039b9e00515863119419e3ab
mohdraqif <mohdraqif94@gmail.com>
2020-04-01 11:39:01 +0530

    1

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Feedback = () => {
   return (
     <div className="feedback">
-      <iframe title="feedback form" width="640px" height= "580px" src= "https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAN__imJQOpUQktDUlBaNERZVDFWVDdNT0ZJSE5aNkxZNy4u&embed=true" frameborder= "0" marginwidth= "0" marginheight= "0" style={{border: "none", "max-width":"100%","max-height":"100vh"}}></iframe>
+      {/* <iframe title="feedback form" width="640px" height= "580px" src= "https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAN__imJQOpUQktDUlBaNERZVDFWVDdNT0ZJSE5aNkxZNy4u&embed=true" frameborder= "0" marginwidth= "0" marginheight= "0" style={{border: "none", "max-width":"100%","max-height":"100vh"}}></iframe> */}
     </div>
   );
 }
