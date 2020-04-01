a7f3f86566d1b5cfff43592cbc38719bf7e7b0d0
mohdraqif <mohdraqif94@gmail.com>
2020-04-01 20:16:47 +0530

    Intro updated

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../App.css'
+import { Link } from 'react-router-dom';
 
 const Intro = () => {
   return (
@@ -8,8 +9,8 @@ const Intro = () => {
           <div className="content">
             <h1>Hello, my name is <span>Mohd. Raqif Warsi.</span> <br/> I'm a <span>Web Designer.</span> I love creating beautiful websites and projects with <span>clean</span> and elegant designs. Come and get in touch with me <span>here.</span></h1>
             <h4>Start the tour to know more</h4>
-            <a href="/start" class="fancy-button bg-gradient1"><span>Get Started</span></a>
-            <a href="/projects" class="fancy-button bg-gradient2"><span>Live Projects</span></a>
+            <Link to="/journey" class="fancy-button bg-gradient1"><span>Get Started</span></Link>
+            <Link to="/projects" class="fancy-button bg-gradient2"><span>Live Projects</span></Link>
           </div>
         </section>
       </div>
