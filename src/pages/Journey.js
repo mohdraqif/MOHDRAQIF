2efe6e6fba38d7c308e5ae34778e61b98b872030
mohdraqif <mohdraqif94@gmail.com>
2020-04-07 01:48:50 +0530

    Journey Setup Complete

--- src/pages/Journey.js
+++ src/pages/Journey.js
@@ -3,15 +3,53 @@ import '../App.css'
 
 const Journey = () => {
   return (
-    <>
-    <div className="journey-bg2"></div>
-    <div className="journey-bg"></div>
-    <div className="journey-bg2"></div>
-    <div className="journey-bg"></div>
-    <div className="journey-bg2"></div>
-    <div className="journey-bg"></div>
-    <div className="journey-bg2"></div>
-    </>
+    <div class="first">
+    <main id="journey">
+      <div class="journey-info">
+      
+      <div class="bio1">
+        <p>Bio-1 I'm Mohd. Raqif Warsi. Web Designs fascinates me. From scratching head amidst simple CSS rules to this far, I trained myself with creativity, persistence and of course, patience to hit the ultimate goal. Here's a hint about me.</p>
+      </div>
+      <div class="job job-1">
+        <h3><i class="fa fa-twitter fa-2x"></i><a href="https://twitter.com/mohdraqif1" target="_blank" rel="noopener noreferrer"> @mohdraqif1</a></h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      </div>
+
+      <div class="bio2">
+        <p>Bio-2! I'm Mohd. Raqif Warsi. Web Designs fascinates me. From scratching head amidst simple CSS rules to this far, I trained myself with creativity, persistence and of course, patience to hit the ultimate goal. Here's a hint about me.</p>
+      </div>
+      <div class="job job-2">
+        <h3><i class="fa fa-facebook fa-2x"></i><a href="https://www.facebook.com/mohd.raqif.5" target="_blank" rel="noopener noreferrer"> @Mohd Raqif</a></h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      </div>
+
+      <div class="bio3">
+        <p>Bio-3! I'm Mohd. Raqif Warsi. Web Designs fascinates me. From scratching head amidst simple CSS rules to this far, I trained myself with creativity, persistence and of course, patience to hit the ultimate goal. Here's a hint about me.</p>
+      </div>
+      <div class="job job-3">
+        <h3><i class="fa fa-facebook fa-2x"></i><a href="https://www.facebook.com/mohd.raqif.5" target="_blank" rel="noopener noreferrer"> @Mohd Raqif</a></h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      </div>
+      
+      <div class="bio4">
+        <p>Bio-4! I'm Mohd. Raqif Warsi. Web Designs fascinates me. From scratching head amidst simple CSS rules to this far, I trained myself with creativity, persistence and of course, patience to hit the ultimate goal. Here's a hint about me.</p>
+      </div>
+      <div class="job job-4">
+        <h3><i class="fa fa-facebook fa-2x"></i><a href="https://www.facebook.com/mohd.raqif.5" target="_blank" rel="noopener noreferrer"> @Mohd Raqif</a></h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      </div>
+
+      <div class="bio5">
+        <p>Bio-5! I'm Mohd. Raqif Warsi. Web Designs fascinates me. From scratching head amidst simple CSS rules to this far, I trained myself with creativity, persistence and of course, patience to hit the ultimate goal. Here's a hint about me.</p>
+      </div>
+      <div class="job job-5">
+        <h3><i class="fa fa-facebook fa-2x"></i><a href="https://www.facebook.com/mohd.raqif.5" target="_blank" rel="noopener noreferrer"> @Mohd Raqif</a></h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      </div>
+
+    </div>
+  </main>
+</div>
   );
 }
 
