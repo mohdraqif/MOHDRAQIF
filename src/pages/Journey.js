282ab7aee2221c8a38c20400c0bb476baa5605e7
mohdraqif <mohdraqif94@gmail.com>
2020-04-07 03:28:40 +0530

    Journey Completed

--- src/pages/Journey.js
+++ src/pages/Journey.js
@@ -4,52 +4,58 @@ import '../App.css'
 const Journey = () => {
   return (
     <div class="first">
-    <main id="journey">
-      <div class="journey-info">
+      <div class="banner"><h2>Started from here</h2></div>
+      <main id="journey">
+        <div class="journey-info">
+        <div class="bio1">
+          <p>Bio-1 (HTML,CSS,Bootstrap,jQuery) I'm Mohd. Raqif Warsi. Web Designs fascinates me. From scratching head amidst simple CSS rules to this far, I trained myself with creativity, persistence and of course, patience to hit the ultimate goal. Here's a hint about me.</p>
+        </div>
+        <div class="job job-1">
+          <img src="https://images.unsplash.com/photo-1472437774355-71ab6752b434?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=80" alt=" "/>
+        </div>
+
+        <div class="bio2">
+          <p>Bio-2 (Javascript and libraries) I'm Mohd. Raqif Warsi. Web Designs fascinates me. From scratching head amidst simple CSS rules to this far, I trained myself with creativity, persistence and of course, patience to hit the ultimate goal. Here's a hint about me.</p>
+        </div>
+        <div class="job job-2">
+        <img src="https://images.unsplash.com/photo-1507721999472-8ed4421c4af2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjExMDk0fQ&auto=format&fit=crop&w=400&q=80" alt=" "/>
+        </div>
+
+        <div class="bio3">
+          <p>Bio-3 (NodeJS and MongoDB Atlas) I'm Mohd. Raqif Warsi. Web Designs fascinates me. From scratching head amidst simple CSS rules to this far, I trained myself with creativity, persistence and of course, patience to hit the ultimate goal. Here's a hint about me.</p>
+        </div>
+        <div class="job job-3">
+          <img src="https://niittrainingprdcdn.azureedge.net/Images/nodejs-520.jpg" alt=" "/>
+        </div>
+        
+        <div class="bio4">
+          <p>Bio-4 (Netlify and Heroku) I'm Mohd. Raqif Warsi. Web Designs fascinates me. From scratching head amidst simple CSS rules to this far, I trained myself with creativity, persistence and of course, patience to hit the ultimate goal. Here's a hint about me.</p>
+        </div>
+        <div class="job job-4">
+          <img src="https://fabiofranchino.com/blog/assets/posts/how-to-deploy-webapp-heroku-netlify/cover.jpg" alt=" "/>
+        </div>
+
+        <div class="bio5">
+          <p>Bio-5 (ReactJS) I'm Mohd. Raqif Warsi. Web Designs fascinates me. From scratching head amidst simple CSS rules to this far, I trained myself with creativity, persistence and of course, patience to hit the ultimate goal. Here's a hint about me.</p>
+        </div>
+        <div class="job job-5">
+          <img src="https://www.freecodecamp.org/news/content/images/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png" alt=" "/>
+        </div>
+
+        <div class="bio6">
+          <p>Bio-6 (Ethereum & Solidity) I'm Mohd. Raqif Warsi. Web Designs fascinates me. From scratching head amidst simple CSS rules to this far, I trained myself with creativity, persistence and of course, patience to hit the ultimate goal. Here's a hint about me.</p>
+        </div>
+        <div class="job job-6">
+          <img src="https://cia.news/wp-content/uploads/2019/03/La-Blockchain-Ethereum-1000x675.jpg" alt=" "/>
+        </div>
+
+        <div class="bio7">
+          <p>Bio-6 (up and beyond) I'm Mohd. Raqif Warsi. Web Designs fascinates me. From scratching head amidst simple CSS rules to this far, I trained myself with creativity, persistence and of course, patience to hit the ultimate goal. Here's a hint about me. I'm Mohd. Raqif Warsi. Web Designs fascinates me. From scratching head amidst simple CSS rules to this far, I trained myself with creativity, persistence and of course, patience to hit the ultimate goal.</p>
+        </div>
+      </div>
       
-      <div class="bio1">
-        <p>Bio-1 I'm Mohd. Raqif Warsi. Web Designs fascinates me. From scratching head amidst simple CSS rules to this far, I trained myself with creativity, persistence and of course, patience to hit the ultimate goal. Here's a hint about me.</p>
-      </div>
-      <div class="job job-1">
-        <h3><i class="fa fa-twitter fa-2x"></i><a href="https://twitter.com/mohdraqif1" target="_blank" rel="noopener noreferrer"> @mohdraqif1</a></h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-      </div>
-
-      <div class="bio2">
-        <p>Bio-2! I'm Mohd. Raqif Warsi. Web Designs fascinates me. From scratching head amidst simple CSS rules to this far, I trained myself with creativity, persistence and of course, patience to hit the ultimate goal. Here's a hint about me.</p>
-      </div>
-      <div class="job job-2">
-        <h3><i class="fa fa-facebook fa-2x"></i><a href="https://www.facebook.com/mohd.raqif.5" target="_blank" rel="noopener noreferrer"> @Mohd Raqif</a></h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-      </div>
-
-      <div class="bio3">
-        <p>Bio-3! I'm Mohd. Raqif Warsi. Web Designs fascinates me. From scratching head amidst simple CSS rules to this far, I trained myself with creativity, persistence and of course, patience to hit the ultimate goal. Here's a hint about me.</p>
-      </div>
-      <div class="job job-3">
-        <h3><i class="fa fa-facebook fa-2x"></i><a href="https://www.facebook.com/mohd.raqif.5" target="_blank" rel="noopener noreferrer"> @Mohd Raqif</a></h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-      </div>
-      
-      <div class="bio4">
-        <p>Bio-4! I'm Mohd. Raqif Warsi. Web Designs fascinates me. From scratching head amidst simple CSS rules to this far, I trained myself with creativity, persistence and of course, patience to hit the ultimate goal. Here's a hint about me.</p>
-      </div>
-      <div class="job job-4">
-        <h3><i class="fa fa-facebook fa-2x"></i><a href="https://www.facebook.com/mohd.raqif.5" target="_blank" rel="noopener noreferrer"> @Mohd Raqif</a></h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-      </div>
-
-      <div class="bio5">
-        <p>Bio-5! I'm Mohd. Raqif Warsi. Web Designs fascinates me. From scratching head amidst simple CSS rules to this far, I trained myself with creativity, persistence and of course, patience to hit the ultimate goal. Here's a hint about me.</p>
-      </div>
-      <div class="job job-5">
-        <h3><i class="fa fa-facebook fa-2x"></i><a href="https://www.facebook.com/mohd.raqif.5" target="_blank" rel="noopener noreferrer"> @Mohd Raqif</a></h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-      </div>
-
-    </div>
-  </main>
-</div>
+    </main>
+  </div>
   );
 }
 
