f97f941064a62b76f4e6b9f2782846ec8e741b2a
mohdraqif <mohdraqif94@gmail.com>
2020-04-06 21:57:20 +0530

    Projects Updated

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,12 +6,37 @@ const Projects = () => {
   <section class="section">
   <div class="banner"><h2>Our Best Projects</h2></div>
     <div class="grid">
-      <div class="item item--medium" title="Tech Store"></div>
-      <div class="item item--medium" title="Beach Resort"></div>
-      <div class="item item--medium" title="Vintage Technologies"></div>
-      <div class="item item--medium" title="To-Do List"></div>
-      <div class="item item--medium" title="Get Linked"></div>
-      <div class="item item--medium" title="Budget Calculator"></div>
+      <div class="item item--medium">
+        <article className="room">
+          <p className="project-name">Tech Store</p>
+        </article>
+      </div>
+      <div class="item item--medium">
+        <article className="room">
+          <p className="project-name">Tech Store</p>
+        </article>
+      </div>
+      <div class="item item--medium">
+        <article className="room">
+          <p className="project-name">Tech Store</p>
+        </article>
+      </div>
+      <div class="item item--medium">
+        <article className="room">
+          <p className="project-name">Tech Store</p>
+        </article>
+      </div>
+      <div class="item item--medium">
+        <article className="room">
+          <p className="project-name">Tech Store</p>
+        </article>
+      </div>
+      <div class="item item--medium">
+        <article className="room">
+          <p className="project-name">Tech Store</p>
+        </article>
+      </div>
+
     </div>
   </section>
   );
