4f04eb791b27eb735e8adbdb3bf01b9085ea4146
mohdraqif <mohdraqif94@gmail.com>
2020-04-07 05:19:13 +0530

    Journey Completed Final

--- src/pages/Journey.js
+++ src/pages/Journey.js
@@ -8,49 +8,49 @@ const Journey = () => {
       <main id="journey">
         <div class="journey-info">
         <div class="bio1">
-          <p>Bio-1 (HTML,CSS,Bootstrap,jQuery) I'm Mohd. Raqif Warsi. Web Designs fascinates me. From scratching head amidst simple CSS rules to this far, I trained myself with creativity, persistence and of course, patience to hit the ultimate goal. Here's a hint about me.</p>
+          <p><span>H</span>TML & CSS. These two brothers for life met me at the commencement of my path. One simple and the other difficult to perceive. There's only one way to understand CSS, experiment it. Bootstrap provided a better design than HTML and did impressive with jQuery besides. That was smooth!</p>
         </div>
         <div class="job job-1">
           <img src="https://images.unsplash.com/photo-1472437774355-71ab6752b434?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=80" alt=" "/>
         </div>
 
         <div class="bio2">
-          <p>Bio-2 (Javascript and libraries) I'm Mohd. Raqif Warsi. Web Designs fascinates me. From scratching head amidst simple CSS rules to this far, I trained myself with creativity, persistence and of course, patience to hit the ultimate goal. Here's a hint about me.</p>
+          <p><span>J</span>avascript and libraries. JS never slips to amaze us. With a huge authority over the development network, I must declare that this is my star language in the long run. Be it frontend or backend, JS is all I wanted for the development purpose. Go on and change my mind if JS is not a hero for you.</p>
         </div>
         <div class="job job-2">
         <img src="https://images.unsplash.com/photo-1507721999472-8ed4421c4af2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjExMDk0fQ&auto=format&fit=crop&w=400&q=80" alt=" "/>
         </div>
 
         <div class="bio3">
-          <p>Bio-3 (NodeJS and MongoDB Atlas) I'm Mohd. Raqif Warsi. Web Designs fascinates me. From scratching head amidst simple CSS rules to this far, I trained myself with creativity, persistence and of course, patience to hit the ultimate goal. Here's a hint about me.</p>
+          <p><span>N</span>odeJS and MongoDB. With NodeJS on your side, the backend is fun. This Javascript engine endures the ultimate power to deal swiftly with the data and databases like MongoDB and others. Hats off to MongoDB Atlas for such an effortless and secure data storage and access services.</p>
         </div>
         <div class="job job-3">
           <img src="https://niittrainingprdcdn.azureedge.net/Images/nodejs-520.jpg" alt=" "/>
         </div>
         
         <div class="bio4">
-          <p>Bio-4 (Netlify and Heroku) I'm Mohd. Raqif Warsi. Web Designs fascinates me. From scratching head amidst simple CSS rules to this far, I trained myself with creativity, persistence and of course, patience to hit the ultimate goal. Here's a hint about me.</p>
+          <p><span>N</span>etlify and Heroku. Now, how to deploy? Never worry, Heroku and Netlify are the most suitable options to deal in the deployment of your apps and websites (without a domain). Just create an account, connect to GitHub and watch your apps run live! Of course, there are plenty more of the services like Digital Ocean and all.</p>
         </div>
         <div class="job job-4">
           <img src="https://fabiofranchino.com/blog/assets/posts/how-to-deploy-webapp-heroku-netlify/cover.jpg" alt=" "/>
         </div>
 
         <div class="bio5">
-          <p>Bio-5 (ReactJS) I'm Mohd. Raqif Warsi. Web Designs fascinates me. From scratching head amidst simple CSS rules to this far, I trained myself with creativity, persistence and of course, patience to hit the ultimate goal. Here's a hint about me.</p>
+          <p><span>H</span>ere comes the React. Whatever this stuff is, it's fantastic. I can't tell you enough what not I can do with React in my hand. Really. Again a library of JS powered so high that it can do almost everything. Components for frontend, Contentful for content, Strapi for backend and what else you need?</p>
         </div>
         <div class="job job-5">
           <img src="https://www.freecodecamp.org/news/content/images/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png" alt=" "/>
         </div>
 
         <div class="bio6">
-          <p>Bio-6 (Ethereum & Solidity) I'm Mohd. Raqif Warsi. Web Designs fascinates me. From scratching head amidst simple CSS rules to this far, I trained myself with creativity, persistence and of course, patience to hit the ultimate goal. Here's a hint about me.</p>
+          <p><span>A</span>long the journey, I met with Blockchain. A whole new concept to me. Smart contracts, ProofOfWork, Hashing, Cryptocurrency, and all the stuff of the blockchain network. Ethereum, being the central network of the blockchain was a good skill to earn. I hope to continue with my ongoing Ethereum project with all my energy.</p>
         </div>
         <div class="job job-6">
           <img src="https://cia.news/wp-content/uploads/2019/03/La-Blockchain-Ethereum-1000x675.jpg" alt=" "/>
         </div>
 
         <div class="bio7">
-          <p>Bio-6 (up and beyond) I'm Mohd. Raqif Warsi. Web Designs fascinates me. From scratching head amidst simple CSS rules to this far, I trained myself with creativity, persistence and of course, patience to hit the ultimate goal. Here's a hint about me. I'm Mohd. Raqif Warsi. Web Designs fascinates me. From scratching head amidst simple CSS rules to this far, I trained myself with creativity, persistence and of course, patience to hit the ultimate goal.</p>
+          <p><span>S</span>o from scratching head amidst simple CSS rules to this far, I trained myself with creativity, persistence and patience to hit the ultimate goal. Never thinking of withdrawing to this, I will continue my journey till beyond. I hope I'll learn new things from my mistakes and make new connections to other people as well during the journey. It was nice to have you here. Visit again.</p>
         </div>
       </div>
       
